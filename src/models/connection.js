7dcaad0e994f8a54e48ba96addec46788fd8b164
aRON <aronviniciusnascimento@gmail.com>
2024-02-20 17:23:48 -0300

    fix: create table

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,12 +1,11 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-
 const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'todolist',
+  host: process.env.MYSQL_HOST || 'localhost',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'root',
+  database: process.env.MYSQL_DATABASE || 'todolist',
 });
 
 module.exports = connection;
